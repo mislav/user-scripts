6dc162b3b5021c7b454d6c0810576a6fdb46f3bb
Mislav Marohnić <mislav.marohnic@gmail.com>
2008-10-02 02:50:51 +0200

    bump up scriptLength

--- endless_tweets/endless_tweets.user.js
+++ endless_tweets/endless_tweets.user.js
@@ -367,7 +367,7 @@ var wrapper = find(null, '#content > div.wrapper')
 if (wrapper && typeof GM_xmlhttpRequest == "function") {
   var scriptURL = 'http://userscripts.org/scripts/show/24398',
       sourceURL = scriptURL.replace(/show\/(\d+)$/, 'source/$1.user.js'),
-      scriptLength = 20130,
+      scriptLength = 20752,
       updateAvailable = getValue('updateAvailable', false)
 
   function validateScriptLength(length) {
