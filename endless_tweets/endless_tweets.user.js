e913114c89d9383aaf5bc3eea75f14fc278d3c68
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-02-28 19:26:05 +0100

    relativeTime() -> Time.agoInWords() vs. Time.agoToDate()

--- endless_tweets/endless_tweets.user.js
+++ endless_tweets/endless_tweets.user.js
@@ -530,7 +530,7 @@ var buildUpdateFromJSON = (function() {
       preparedData = {
         id: data.id,
         username: data.user.screen_name, avatar: data.user.profile_image_url, real_name: data.user.name,
-        created_at: date.toString(), created_ago: relativeTime(date) + ' ago',
+        created_at: date.toString(), created_ago: Time.agoInWords(date) + ' ago',
         text: twitterLinkify(data.text), source: data.source,
         in_reply_to: data.in_reply_to_screen_name, in_reply_to_status: data.in_reply_to_status_id,
         fav_action: data.favorited ? 'un-favorite' : 'favorite',
@@ -941,21 +941,34 @@ function loadJSON(url, onload, params) {
   return ajax(params)
 }
 
-// stolen from twitter.com (hope you guys don't mind)
-function relativeTime(date, relativeTo) {
-  if (!relativeTo) relativeTo = new Date
-  var delta = (relativeTo.getTime() - date.getTime()) / 1000
-  if (delta < 5) return 'less than 5 seconds'
-  else if (delta < 10)  return 'less than 10 seconds'
-  else if (delta < 20)  return 'less than 20 seconds'
-  else if (delta < 60)  return 'less than a minute'
-  else if (delta < 120) return 'about a minute'
-  else if (delta < (60*60))    return Math.round(delta / 60) + ' minutes'
-  else if (delta < (120*60))   return 'about an hour'
-  else if (delta < (24*60*60)) return 'about ' + Math.round(delta / 3600) + ' hours'
-  else if (delta < (48*60*60)) return '1 day'
-  else return Math.round(delta / 86400) + ' days'
-}
+var Time = (function() {
+  var sec = { s: 1, m: 60, h: 60 * 60, d: 24 * 60 * 60 }
+  
+  return {
+    agoInWords: function(time, relativeTo) {
+      if (!relativeTo) relativeTo = new Date()
+      var delta = (relativeTo - time) / 1000
+      if (delta < 5) return 'less than 5 seconds'
+      else if (delta < 10)  return 'less than 10 seconds'
+      else if (delta < 20)  return 'less than 20 seconds'
+      else if (delta < sec.m)  return 'less than a minute'
+      else if (delta < sec.m * 2) return 'about a minute'
+      else if (delta < sec.h)    return Math.round(delta / 60) + ' minutes'
+      else if (delta < sec.h * 2)   return 'about an hour'
+      else if (delta < sec.d) return 'about ' + Math.round(delta / 3600) + ' hours'
+      else if (delta < sec.d * 2) return '1 day'
+      else return Math.round(delta / sec.d) + ' days'
+    },
+    agoToDate: function(string, relativeTo) {
+      if (!relativeTo) relativeTo = new Date()
+      var match = string.match(/(?:(?:about|less than) )?(a|an|\d+) ([smhd])/)
+      if (match) {
+        var amount = Number(match[1]) || 1, metric = match[2]
+        return new Date(relativeTo - sec[metric] * amount * 1000)
+      }
+    }
+  }
+})()
 
 function strip(string) {
   return string.replace(/^\s+/, '').replace(/\s+$/, '')
