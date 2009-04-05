89460c290956d82e7bef126adba8d748e69003cd
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-04-05 17:49:40 +0200

    release 0.9.6

--- endless_tweets/endless_tweets.user.js
+++ endless_tweets/endless_tweets.user.js
@@ -67,8 +67,8 @@ var timeline = $('timeline'),
     home = 'home' == currentPage,
     singleTweetPage = 'show' == currentPage,
     sourceString = 'endlesstweets',
-    scriptVersion = '0.9.5',
-    scriptLength = 37507
+    scriptVersion = '0.9.6',
+    scriptLength = 37585
 
 if (home) {
   var lastReadTweet = getValue('lastReadTweet', 0),
